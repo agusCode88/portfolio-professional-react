2b9d0e08be1f03ba0762e10208376b152758ccfb
agusCode88 <agus.romero.salazar@gmail.com>
2023-07-20 12:16:32 -0400

    Personalización del sitio
    Primeros cambios con información real del portafolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../assets/logo.svg';
+import Logo1 from '../assets/aguscode.png';
 
 const Header = () => {
   return <header className='py-8'>
@@ -8,7 +9,7 @@ const Header = () => {
       <div className='flex justify-between items-center'>
         {/*logo*/}
         <a href='#'>
-           <img src={Logo} alt=''></img>
+           <img src={Logo1} alt=''></img>
         </a>
         <button className='btn btn-sm'> Work with me!</button>
       </div>
